faf0efa13f4ce5053aa788d34c6a059d0feb8a81
skillganon <56158382+skillganon@users.noreply.github.com>
2019-11-04 08:44:12 +0100

    Update world.js

--- Labb 1 inlämmning/world.js
+++ Labb 1 inlämmning/world.js
@@ -1,6 +1,6 @@
 
 alert('welcome to the new world')
-var gender = prompt('pick between Male of Female')
+var gender = prompt('pick between Male or Female')
 
 
 
@@ -20,7 +20,7 @@ else{
 
 alert('after reciving your weapon you decide to explore the world.')
 alert('while you are exploring the forest of this new world you run into bandits!')
-var action = prompt('Would you like to fight this bandits? Yes, No')
+var action = prompt('Would you like to fight this bandits? Yes or No')
 
 if(action == 'Yes'){
     alert('you enter combat where you end up killing the bandits')
@@ -36,12 +36,29 @@ else{
     startGame();
 }
 alert('after searching for 2 days you find a town and decide to enter and speak to the healer of that city')
-var answer = prompt('healer Nera says: would you like me to Heal you?')
+var answer = prompt('healer Nera says: would you like me to Heal you? Yes or No')
 
 if(answer == 'Yes') {
     alert('you are fully healed')
 }
-else {
-    alert('Nera: i notice that you are heavily wounded and i will heal you so you dont die.!')
+else if(answer == 'No')  {
+    alert('you start to feel weak , dizzy from the wound that you didnt heal and you pass out')
+    alert ('Nera discovers your body unconscious on the floor, she heals your wounds and then carries you to a bed where you rest untill you wake up')
 }
-    
+    else {
+        alert('Nera looks at you as you collapse on the floor not being able to decide if to get healed or not. Nera heals you up and carries you to a bed where you rest untill you wake up')
+
+    }
+alert('you wake up fully restored in a room with a chest in it.')
+var choice = prompt('would you like to loot the chest Yes or No')
+
+if(choice == 'Yes') {
+    alert('you found a map of the forest and city')
+}
+  else if ('No') {
+      alert('you leave the room and go to the shop to buy a map of the area you are in.')
+
+  }  
+  else{
+      alert('undecesive about what to do you decide to go out of the town. The healer nera suddenly appears and gives you a map of the city and the forest so you can find your way back if you ever need to heal up again.')
+  }
